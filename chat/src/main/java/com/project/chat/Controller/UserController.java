06ee81f500f50d14b30f38cbdbdd763062e1debd
LeeYeJi <lwj1748@naver.com>
2020-08-23 22:26:09 +0900

    [Commit] social login - kakao

--- chat/src/main/java/com/project/chat/Controller/UserController.java
+++ chat/src/main/java/com/project/chat/Controller/UserController.java
@@ -1,24 +1,34 @@
 package com.project.chat.Controller;
 
+import java.util.HashMap;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.chat.Service.KakaoUserService;
 import com.project.chat.Service.UserService;
 import com.project.chat.Vo.User;
 
+import lombok.extern.slf4j.Slf4j;
 
 
+@Slf4j
 @RestController
 public class UserController {
 	
 	@Autowired
 	UserService userService;
+	@Autowired
+	KakaoUserService kakaoUserService;
 	
 	@RequestMapping(value = "/getUsers", method = RequestMethod.GET)
 	public List<User> getUsers() throws Exception {
@@ -44,5 +54,57 @@ public class UserController {
 	public int deleteUser(String id) throws Exception {
 		return userService.deleteUser(id);
 	}
-
+	
+	@RequestMapping(value = "/login/social/kakao")
+	public int oauthKakaoLogin(@RequestParam("code") String code, HttpSession session) throws Exception {
+		// 1. Client쪽에 카카오로그인 페이지를 띄우고 동의하기 버튼을 누르면 해당 URI로 Code를 포함하여 Redirect됨
+		
+		log.info("code : " + code);
+		
+		// 2. code를 기반으로 사용자 token받아오기
+		String access_Token = kakaoUserService.getAccessToken(code);
+		
+		// 3. token을 기반으로 사용자 정보 받아와서 DB정보와 비교, 없으면 insert
+		User userInfo = kakaoUserService.getUserInfo(access_Token);
+		
+		log.info("userInfo : " + userInfo);
+		
+		if(userInfo == null)
+			return -1;
+		else
+			return 1;
+	}
+	
+	@RequestMapping(value = "/logout/social/kakao/simple")
+	public int oauthKakaoLogout(HttpSession session) throws Exception {
+		// 서비스 로그아웃
+		int result = kakaoUserService.kakaoLogout((String)session.getAttribute("access_Token"));
+		
+		log.info("Logout - userId : " + session.getAttribute("userId"));
+	    session.removeAttribute("access_Token");
+	    session.removeAttribute("userEmail");
+	    session.removeAttribute("userId");
+	    
+		return result;
+	}
+	
+	@RequestMapping(value = "/logout/social/kakao")
+	public int oauthKakaoLogout2(/* @RequestParam("state") String state */) throws Exception {
+		// 카카오계정과 함께 로그아웃 기능제공
+		log.info("oauthKakaoLogout2");
+		//log.info("state : " + state);
+		
+		return 1;
+	}
+	
+	@RequestMapping(value = "/session")
+	public String getSessionInfo(HttpSession session) throws Exception {
+		// 임시 테스트용 Session정보 확인
+		log.info("session - userId : " + session.getAttribute("userId"));
+		log.info("session - userEmail : " + session.getAttribute("userEmail"));
+		log.info("session - access_Token : " + session.getAttribute("access_Token"));
+		
+		return session.toString();
+	}
+	
 }
---
+++ chat/src/main/java/com/project/chat/Service/KakaoUserService.java
@@ -0,0 +1,217 @@
+package com.project.chat.Service;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.project.chat.Mapper.UserMapper;
+import com.project.chat.Vo.User;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class KakaoUserService {
+	
+	@Autowired 
+	UserMapper userMapper;
+	
+	@Value("${spring.url.base}")
+	private String baseUrl;
+	
+	@Value("${spring.social.kakao.client_id}")
+	private String kakaoClientId;
+	
+	@Value("${spring.social.kakao.login_redirect}")
+	private String kakaoLoginRedirect;
+	
+	@Value("${spring.social.kakao.logout_redirect}")
+	private String kakaoLogoutRedirect;
+	
+	@Value("${spring.social.kakao.url.token}")
+	private String kakaoUrlToken;
+	
+	@Value("${spring.social.kakao.url.profile}")
+	private String kakaoUrlProfile;
+	
+	@Value("${spring.social.kakao.url.logout}")
+	private String kakaoUrlLogout;
+	
+	public String getAccessToken (String authorize_code) {
+        String access_Token = "";
+        String refresh_Token = "";
+	    
+        try {
+            URL url = new URL(kakaoUrlToken);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            
+            //    POST 요청을 위해 기본값이 false인 setDoOutput을 true로
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            
+            //    POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+            StringBuilder loginUrl = new StringBuilder()
+    				.append("grant_type=authorization_code")
+    				.append("&client_id=").append(kakaoClientId)
+    				.append("&redirect_uri=").append(baseUrl).append(kakaoLoginRedirect)
+    				.append("&code=").append(authorize_code);
+            bw.write(loginUrl.toString());
+            bw.flush();
+            
+            //    결과 코드가 200이라면 성공
+            int responseCode = conn.getResponseCode();
+            log.info("responseCode : " + responseCode);
+            
+            // 200이 아닌경우 예외처리?
+            if(responseCode != 200) {
+            	// 토큰 정보가 잘못된 형식 => HTTP Status 400에 code -2가 반환
+            	// 토큰 만료 => HTTP Status 401에 code -401 (갱신 필요)
+            	// 응답 에러 코드가 -1이라면 카카오 플랫폼 서비스의 일시적 내부 장애 상태
+            	// 이 외의 에러는 앱이나 사용자, 토큰 등의 상태가 더이상 유효하지 않아 쓸 수 없는 경우 (로그아웃 처리)
+            	return null;
+            }
+ 
+            //    요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
+            
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+            log.info("response body : " + result);
+            
+            //    Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+            
+            access_Token = element.getAsJsonObject().get("access_token").getAsString();
+            refresh_Token = element.getAsJsonObject().get("refresh_token").getAsString();
+            
+            log.info("access_token : " + access_Token);
+            log.info("refresh_token : " + refresh_Token);
+            
+            br.close();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } 
+        
+        return access_Token;
+    }
+	
+	public User getUserInfo (String access_Token) {
+	    // 요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
+	    //HashMap<String, Object> userInfo = new HashMap<>();
+	    User userInfo = new User();
+	    
+	    try {
+	        URL url = new URL(kakaoUrlProfile);
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.setRequestMethod("POST");
+	        
+	        // 요청에 필요한 Header에 포함될 내용
+	        conn.setRequestProperty("Authorization", "Bearer " + access_Token);
+	        
+	        int responseCode = conn.getResponseCode();
+	        log.info("responseCode : " + responseCode);
+
+            // 200이 아닌경우 예외처리?
+            if(responseCode != 200) {
+            	// 토큰 정보가 잘못된 형식 => HTTP Status 400에 code -2가 반환
+            	// 토큰 만료 => HTTP Status 401에 code -401 (갱신 필요)
+            	// 응답 에러 코드가 -1이라면 카카오 플랫폼 서비스의 일시적 내부 장애 상태
+            	// 이 외의 에러는 앱이나 사용자, 토큰 등의 상태가 더이상 유효하지 않아 쓸 수 없는 경우 (로그아웃 처리)
+            	return null;
+            }
+	        
+	        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	        
+	        String line = "";
+	        String result = "";
+	        
+	        while ((line = br.readLine()) != null) {
+	            result += line;
+	        }
+	        log.info("response body : " + result);
+	        
+	        JsonParser parser = new JsonParser();
+	        JsonElement element = parser.parse(result);
+	        
+	        JsonObject properties = element.getAsJsonObject().get("properties").getAsJsonObject();
+	        JsonObject kakao_account = element.getAsJsonObject().get("kakao_account").getAsJsonObject();
+	        
+	        String id = element.getAsJsonObject().get("id").toString();
+	        String nickname = properties.getAsJsonObject().get("nickname").getAsString();
+	        String email = kakao_account.getAsJsonObject().get("email").getAsString();
+	        
+		    userInfo.setId(id);
+		    userInfo.setPw(id);
+		    userInfo.setType("kakao");
+		    if(email != null && email != "")
+		    	userInfo.setEmail(email);
+	        
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	    
+	    int result = userMapper.checkUser(userInfo);
+	    
+	    if(result == 1) {
+	    	// 로그인 성공 -> JWT토큰 발급?
+	    	log.info("기존회원 로그인");
+	    }else if(result == 0) {
+	    	// 신규회원 DB insert
+	    	log.info("신규회원 로그인");
+	    	userMapper.postUser(userInfo);
+	    }
+	    
+	    return userInfo;
+	}
+	
+	public int kakaoLogout(String access_Token) {
+	    String reqURL = kakaoUrlLogout;
+	    try {
+	        URL url = new URL(reqURL);
+	        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+	        conn.setRequestMethod("POST");
+	        conn.setRequestProperty("Authorization", "Bearer " + access_Token);
+	        
+	        int responseCode = conn.getResponseCode();
+	        log.info("responseCode : " + responseCode);
+	        
+	        if(responseCode != 200) {
+	        	return -1;
+	        }
+	        
+	        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+	        
+	        String result = "";
+	        String line = "";
+	        
+	        while ((line = br.readLine()) != null) {
+	            result += line;
+	        }
+	        log.info(result);
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    } finally {
+	    	return 1;
+	    }
+	}
+
+}
